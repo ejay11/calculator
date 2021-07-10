e0494626fea2e042d644a59168de40455a9b8770
ejay11 <ellen.jaquette@gmail.com>
2021-07-09 21:44:24 -0500

    initial math functions and calculator container added

---
+++ calculator.js
@@ -0,0 +1,17 @@
+// --CALCULATOR MATH FUNCTIONS--
+
+const add = ((num1, num2) => {
+    return num1 + num2;
+});
+
+const subtract = ((num1, num2) => {
+    return num1 - num2;
+});
+
+const multiply = ((num1, num2) => {
+    return num1 * num2;
+});
+
+const divide = ((num1, num2) => {
+    return num1 / num2;
+});
