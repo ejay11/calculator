c8872e5120476685dd1ebec761e41033ee80d4e5
ejay11 <ellen.jaquette@gmail.com>
2021-07-10 21:11:21 -0500

    operate function and calculator buttons done

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# calculator
+# Calculator
+A Calculator made with Javascript, following <a href="https://www.theodinproject.com">The Odin Project's</a> curriculum.
+
+https://www.theodinproject.com/paths/foundations/courses/foundations/lessons/calculator
--- calculator.js
+++ calculator.js
@@ -15,3 +15,18 @@ const multiply = ((num1, num2) => {
 const divide = ((num1, num2) => {
     return num1 / num2;
 });
+
+const operate =((operator, num1, num2) => {
+    if (operator == '+'){
+        add(num1, num2);
+    }
+    else if (operator == '-'){
+        subtract (num1, num2);
+    }
+    else if (operator == 'x'){
+        multiply(num1, num2);
+    }
+    else (operator == '/')
+        divide (num1, num2);
+    
+    })
