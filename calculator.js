4d26c732380ff12063f3fb7c54512fa5019df63f
ejay11 <ellen.jaquette@gmail.com>
2021-07-14 17:21:58 -0500

    event listeners and clear button added

--- calculator.js
+++ calculator.js
@@ -16,17 +16,29 @@ const divide = ((num1, num2) => {
     return num1 / num2;
 });
 
-const operate =((operator, num1, num2) => {
-    if (operator == '+'){
-        add(num1, num2);
-    }
-    else if (operator == '-'){
-        subtract (num1, num2);
-    }
-    else if (operator == 'x'){
-        multiply(num1, num2);
-    }
-    else (operator == '/')
-        divide (num1, num2);
-    
-    })
+const operate = ((operator, num1, num2) => {
+    if (operator == '+') {
+        return add(num1, num2);
+    } else if (operator == '-') {
+        return subtract(num1, num2);
+    } else if (operator == 'x') {
+        return multiply(num1, num2);
+    } else(operator == '/')
+    return divide(num1, num2);
+
+})
+//--CALCULATOR DISPLAY FUNCTIONS--
+
+const displayText = ((buttonClicked) => {
+    let value = buttonClicked;
+    document.getElementById(display).innerHTML = value;
+
+})
+
+const sayHello =(() => {
+    console.log("working")
+    alert ('Hello');
+})
+let buttons = document.getElementsByClassName('calc-btn');
+buttons = Array.from(buttons);
+buttons.forEach((button) => {button.addEventListener('click', sayHello)});
